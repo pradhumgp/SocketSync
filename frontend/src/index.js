f15b49d8ec39a6bb905a6290012e64fb8bc02200
pradhumgp <pradhumgp@gmail.com>
2024-06-07 23:13:49 +0530

    context setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <Router>
-    <ChakraProvider>
-    <App/>
-    </ChakraProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Router>
-  </React.StrictMode>
+  </ChakraProvider>
 );
